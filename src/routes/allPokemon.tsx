a1130a0dbd7d54b67f45a63e87e98e146db53d6d
peterkruhoffer <peter.kruhoffer@gmail.com>
2025-08-02 13:02:54 +0200

    Update text size

--- src/routes/allPokemon.tsx
+++ src/routes/allPokemon.tsx
@@ -1,67 +1,78 @@
-import { useQuery } from "@tanstack/solid-query"
-import { createMemo, createSignal, For, Match, onCleanup, Show, Suspense, Switch } from "solid-js"
-import { getAllPokemonFromDB, MyPokemon } from "~/server/pokemonApi"
+import { useQuery } from "@tanstack/solid-query";
+import {
+  createMemo,
+  createSignal,
+  For,
+  Match,
+  onCleanup,
+  Show,
+  Suspense,
+  Switch,
+} from "solid-js";
+import { getAllPokemonFromDB, MyPokemon } from "~/server/pokemonApi";
 
 type colorMatch = {
-  bg: string
-  color: string
-}
+  bg: string;
+  color: string;
+};
 
 const typeColors: Record<string, colorMatch> = {
-  normal: { bg: '#A8A77A', color: "black" },
-  fire: { bg: '#EE8130', color: "black" },
-  water: { bg: '#6390F0', color: "black" },
-  electric: { bg: '#F7D02C', color: "black" },
-  grass: { bg: '#7AC74C', color: 'black' },
-  ice: { bg: '#96D9D6', color: "black" },
-  fighting: { bg: '#C22E28', color: "white" },
-  poison: { bg: '#A33EA1', color: "white" },
-  ground: { bg: '#E2BF65', color: "black" },
-  flying: { bg: '#A98FF3', color: "black" },
-  psychic: { bg: '#F95587', color: "black" },
-  bug: { bg: '#A6B91A', color: "black" },
-  rock: { bg: '#B6A136', color: "black" },
-  ghost: { bg: '#735797', color: "white" },
-  dragon: { bg: '#6F35FC', color: "white" },
-  dark: { bg: '#705746', color: "white" },
-  steel: { bg: '#B7B7CE', color: "black" },
-  fairy: { bg: '#D685AD', color: "black" }
-}
+  normal: { bg: "#A8A77A", color: "black" },
+  fire: { bg: "#EE8130", color: "black" },
+  water: { bg: "#6390F0", color: "black" },
+  electric: { bg: "#F7D02C", color: "black" },
+  grass: { bg: "#7AC74C", color: "black" },
+  ice: { bg: "#96D9D6", color: "black" },
+  fighting: { bg: "#C22E28", color: "white" },
+  poison: { bg: "#A33EA1", color: "white" },
+  ground: { bg: "#E2BF65", color: "black" },
+  flying: { bg: "#A98FF3", color: "black" },
+  psychic: { bg: "#F95587", color: "black" },
+  bug: { bg: "#A6B91A", color: "black" },
+  rock: { bg: "#B6A136", color: "black" },
+  ghost: { bg: "#735797", color: "white" },
+  dragon: { bg: "#6F35FC", color: "white" },
+  dark: { bg: "#705746", color: "white" },
+  steel: { bg: "#B7B7CE", color: "black" },
+  fairy: { bg: "#D685AD", color: "black" },
+};
 
 export default function AllPokemon() {
-  const [search, setSearch] = createSignal("")
+  const [search, setSearch] = createSignal("");
   const query = useQuery(() => ({
     queryKey: ["all-pokemon"],
     queryFn: () => getAllPokemonFromDB(),
-    staleTime: Infinity
-  }))
+    staleTime: Infinity,
+  }));
 
   const filteredTypes = createMemo(() => {
     return query.data?.filter((d) => {
-      return d.types.includes(search()) || d.name.includes(search())
-    })
-  })
+      return d.types.includes(search()) || d.name.includes(search());
+    });
+  });
 
-  let timeoutId: number | undefined
-  function handleInput(e: InputEvent & {
-    currentTarget: HTMLInputElement;
-  }) {
-    const newValue = e.currentTarget.value
+  let timeoutId: number | undefined;
+  function handleInput(
+    e: InputEvent & {
+      currentTarget: HTMLInputElement;
+    },
+  ) {
+    const newValue = e.currentTarget.value;
 
     if (timeoutId) {
-      clearTimeout(timeoutId)
+      clearTimeout(timeoutId);
     }
 
     timeoutId = window.setTimeout(() => {
-      setSearch(newValue)
-    }, 250)
+      setSearch(newValue);
+    }, 250);
   }
 
   onCleanup(() => {
     if (timeoutId) {
-      clearTimeout(timeoutId)
+      clearTimeout(timeoutId);
     }
-  })
+  });
 
   return (
     <section class="grid grid-cols-12 gap-8 p-10">
@@ -80,25 +91,34 @@ export default function AllPokemon() {
             <PokemonGrid pokemonList={filteredTypes()!} />
           </Show>
           <Switch>
-            <Match when={query.isPending}><p>Loading...</p></Match>
+            <Match when={query.isPending}>
+              <p>Loading...</p>
+            </Match>
             <Match when={query.isError}>Error: {query.error?.message}</Match>
-            <Match when={query.isSuccess && (filteredTypes() === undefined || filteredTypes()?.length === 0)}>
+            <Match
+              when={
+                query.isSuccess &&
+                (filteredTypes() === undefined || filteredTypes()?.length === 0)
+              }
+            >
               <PokemonGrid pokemonList={query.data!} />
             </Match>
           </Switch>
         </div>
       </Suspense>
-    </section >
-  )
+    </section>
+  );
 }
 
 type DebounceInputProps = {
-  searchQuery: string
-  handleInput: (e: InputEvent & {
-    currentTarget: HTMLInputElement;
-    target: HTMLInputElement;
-  }) => void
-}
+  searchQuery: string;
+  handleInput: (
+    e: InputEvent & {
+      currentTarget: HTMLInputElement;
+      target: HTMLInputElement;
+    },
+  ) => void;
+};
 
 function DebounceInput(props: DebounceInputProps) {
   return (
@@ -108,11 +128,10 @@ function DebounceInput(props: DebounceInputProps) {
       placeholder="search types or names"
       class="p-2 w-full border col-span-6 col-start-4"
     />
-  )
+  );
 }
 
-
-type PokemonGridProps = { pokemonList: MyPokemon[] }
+type PokemonGridProps = { pokemonList: MyPokemon[] };
 
 function PokemonGrid(props: PokemonGridProps) {
   return (
@@ -120,23 +139,37 @@ function PokemonGrid(props: PokemonGridProps) {
       <For each={props.pokemonList}>
         {(poke) => (
           <div class="flex flex-col justify-center items-center gap-4 border">
-            <div >
-              <p class="text-lg uppercase text-center">{poke.name}</p>
+            <div>
+              <p class="text-xl uppercase text-center">{poke.name}</p>
               <p class="text-lg uppercase text-center">#{poke.id}</p>
             </div>
             <div class="flex flex-row gap-x-4">
               <For each={poke.types}>
                 {(type) => (
-                  <div class="px-2" style={{ "background-color": typeColors[type.toLowerCase()].bg }}>
-                    <p style={{ "color": typeColors[type.toLowerCase()].color }}>{type}</p>
+                  <div
+                    class="px-3 py-1"
+                    style={{
+                      "background-color": typeColors[type.toLowerCase()].bg,
+                    }}
+                  >
+                    <p
+                      class="text-lg"
+                      style={{ color: typeColors[type.toLowerCase()].color }}
+                    >
+                      {type}
+                    </p>
                   </div>
                 )}
               </For>
             </div>
-            <img src={poke.sprite} alt={poke.name} class="size-52 pixelated border" />
+            <img
+              src={poke.sprite}
+              alt={poke.name}
+              class="size-52 pixelated border"
+            />
           </div>
         )}
       </For>
     </div>
-  )
+  );
 }
